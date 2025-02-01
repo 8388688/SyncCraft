40413e3dab6e02a5e1216d9abf5930e1d4231d07
8388688 <98734038+8388688@users.noreply.github.com>
2025-02-01 20:44:49 +0800

    Delete compile.bat

--- compile.bat
+++
@@ -1,3 +0,0 @@
-@REM set fp=I:\2020\8388688\ClosedSpace\Misc\File\py_files\myworkspace\Zmp-X\Peeker\history\v1.5.0_Release
-set fp=%~dp0
-pyinstaller --noconfirm --onefile --windowed --icon "%fp%\assets\icon.ico" --add-data "%fp%\assets;assets/"  "%fp%\SyncCraft.py"
