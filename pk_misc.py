c266cce3ed2e67874bde56ed99fffa5fd4388d08
8388688 <98734038+8388688@users.noreply.github.com>
2025-03-10 22:19:02 +0800

    Add files via upload

--- pk_misc.py
+++ pk_misc.py
@@ -6,7 +6,7 @@ from time import time, localtime, strftime
 
 import simple_tools as st
 
-__version__ = "1.9.2"
+__version__ = "1.9.2-beta2"
 build_time = 1741449600
 TITLE = "SyncCraft"
 rate_list = ("Bytes", "KB", "MB", "GB", "TB", "PB", "EB")
