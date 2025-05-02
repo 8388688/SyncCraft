3a2329f191513d4bfcd4406d27a3c6ffc4172311
8388688 <MeadEyetoe@proton.me>
2025-05-02 19:53:59 +0800

    On branch dev Changes to be committed: 	new file:   .gitignore 	modified:   sync_con.py

--- sync_con.py
+++ sync_con.py
@@ -1,7 +1,7 @@
-import colorlog, colorama
+import colorlog, os
 import logging
 from abc import ABC, abstractmethod
-from typing import AnyStr
+from typing import AnyStr, Literal
 
 
 class BaseLogging(logging.Logger):
@@ -22,49 +22,55 @@ class BaseLogging(logging.Logger):
     def __init__(self, name: str):
         super().__init__(name)
         logging.addLevelName(self.L_NOTICE, "NOTICE")  # 设置自定义日志级别的名称
-        logging.addLevelName(self.L_NOTICE, "NOTICE")  # 设置自定义日志级别的名称
         self.setLevel(logging.DEBUG)
-        self.general_formatter = logging.Formatter(
-            "[%(asctime)s | %(name)s]%(levelname)s: %(message)s",
+
+        self.console_formatter = colorlog.ColoredFormatter(
+            "%(log_color)s[%(asctime)s.%(msecs)03d] %(filename)s -> %(funcName)s line:%(lineno)d [%(levelname)s] : %(message)s",
+            datefmt=self.ASC_TIME_FORMAT,
+            log_colors={
+                "DEBUG": "white",
+                "INFO": "green",
+                "NOTICE": "cyan",
+                "WARNING": "yellow",
+                "ERROR": "red",
+                "CRITICAL": "bold_red,bg_white",
+            }
+        )
+        self.file_formatter = logging.Formatter(
+            "[%(asctime)s.%(msecs)03d] %(filename)s -> %(funcName)s line:%(lineno)d [%(levelname)s] : %(message)s",
             datefmt=self.ASC_TIME_FORMAT,
         )
-        self.color_formatter = colorlog.ColoredFormatter(
-        '%(log_color)s%(levelname)s: %(message)s',
-        log_colors={
-            'DEBUG': 'cyan',
-            'INFO': 'green',
-            'WARNING': 'yellow',
-            'ERROR': 'red',
-            'CRITICAL': "red,bg_white",
-        }
-    )
 
-        # 使用FileHandler输出到文件
         self.local_log = logging.FileHandler("G:\\Temp\\log.txt", encoding="utf-8")
-        # local_log.setLevel(logging.DEBUG)
-        self.local_log.setFormatter(self.general_formatter)
-        self.global_log = logging.FileHandler("G:\\Temp\\loggg.txt", encoding="utf-8")
-        self.global_log.setFormatter(self.general_formatter)
+        # self.local_log.setLevel(logging.DEBUG)
+        self.local_log.setFormatter(self.file_formatter)
+        self.global_log = logging.FileHandler("G:\\Temp\\glog.txt", encoding="utf-8")
+        self.global_log.setFormatter(self.file_formatter)
 
         # 使用StreamHandler输出到屏幕
         self.console = logging.StreamHandler()
-        # ch.setLevel(logging.DEBUG)
-        self.console.setFormatter(self.general_formatter)
+        # self.console.setLevel(logging.DEBUG)
+        self.console.setFormatter(self.console_formatter)
 
-        self.addHandler(self.console)
-        self.addHandler(self.local_log)
-        self.addHandler(self.global_log)
+        if not self.handlers:
+            self.addHandler(self.console)
+            self.addHandler(self.local_log)
+            self.addHandler(self.global_log)
+        self.console.close()
+        self.local_log.close()
+        self.global_log.close()
 
     # def log(self, *__text, tag, sep=" ", end="\n"):
         # msg = sep.join(str(i) for i in __text) + end
         # super().log(level=tag, msg=msg, *tuple())
 
-    def _notice(self, msg, *args, **kwargs):
+    def notice(self, msg, *args, **kwargs):
         if self.isEnabledFor(self.L_NOTICE):
-            self.log(self.L_NOTICE, msg, *args, **kwargs)
-
-    notice = _notice
-
+            self._log(self.L_NOTICE, msg, args, **kwargs)
+    
+    def commit(self):
+        self.local_log.flush()
+        self.global_log.flush()
 
 
 class Archiver:
@@ -73,34 +79,123 @@ class Archiver:
 
 
 class BaseSynchronization(ABC):
+    sync_type = ""  # 子类必须重写 sync_type 方法，并且此方法对于实例来说为只读
+    def __init__(self, src, dst):
+        self.src = src
+        self.dst = dst
+
+        self.archives = []
+
     @abstractmethod
-    def __init__(self):
-        # raise NotImplementedError("子类必须实现这个方法")
-        pass
+    def run(self):
+        raise NotImplementedError("子类必须实现 run 方法")
+    
+    def get_factor(self, file_path, return_type: Literal["code", "int"]="code") -> dict | int:
+        # return_type: "code" | "dict"
+        #
+        # 专门针对驱动器同步的增强
+        # 一串 N 位二进制数字
+        # 0000000000000000
+        # 第 2^0 位：是否存在？
+        # 第 2^1 位：是否为驱动器？
+        # 第 2^2 位：src 所在驱动器的序列号是否相同？（未实现）
+        # 第 2^3 位：src 所在挂载点的 GUID 是否相同？
+        # 第 2^4 位：src 所在挂载点的序列号是否存在于【黑】名单
+        # 第 2^5 位：src 所在挂载点的序列号是否存在于【白】名单
+        # 第 2^6 位：src 所在挂载点的卷标是否存在于【黑】名单
+        # 第 2^7 位：src 所在挂载点的卷标是否存在于【白】名单
+        cur_exists_ch = 0
+        cur_exists_list = dict()
+
+        if os.path.exists(file_path):
+            cur_exists_ch += 1 * 2 ** 0  # m * n ** p format: 以 n 进制表示的数字串，第 p 位数字为 m
+            cur_exists_list.update({"exists": True})
+        else:
+            cur_exists_ch += 0
+            cur_exists_list.update({"exists": False})
+
+        tmp = self.__label2mountId(file_path)
+        self.record_fx(f"{file_path} 对应的卷 ID：{tmp}")
+        if tmp and tmp != file_path:
+            cur_exists_ch += 1 * 2 ** 1
+            cur_exists_list.update({"ismount": True})
+        else:
+            cur_exists_ch += 0
+            cur_exists_list.update({"ismount": False})
+
+        id_blk = self.cursors[file_path].get("volumeId_blacklist", self.profileSettings.get("volumeId_blacklist", []))
+        if tmp and (not id_blk or tmp in id_blk):
+            cur_exists_ch += 1 * 2 ** 4
+            cur_exists_list.update({"volumeId_in_blacklist": True})
+        else:
+            cur_exists_ch += 0
+            cur_exists_list.update({"volumeId_in_blacklist": False})
+        if tmp and tmp in self.cursors[file_path].get(
+                "volumeId_whitelist", self.profileSettings.get("volumeId_whitelist", [])):
+            cur_exists_ch += 1 * 2 ** 5
+            cur_exists_list.update({"volumeId_in_whitelist": True})
+        else:
+            cur_exists_ch += 0
+            cur_exists_list.update({"volumeId_in_whitelist": False})
+
+        if self.cursors[file_path]["lastrun"] != tmp:
+            cur_exists_ch += 0
+            cur_exists_list.update({"samemount": False})
+            self.record_fx(f"检测到不同的卷序列号 - {self.cursors[file_path]['lastrun']} ≠ {tmp}")
+            self.__rename_and_register(self.cursors[file_path]["dst"], file_path)
+        else:
+            cur_exists_ch += 1 * 2 ** 3
+            cur_exists_list.update({"samemount": True})
+            self.record_fx(f"相同的挂载点 - {tmp}")
+        self.cursors[file_path].update({"lastrun": tmp})
+        # self.record_fx(f"{cur_exists_list}")
+        if cur_exists_list.get("exists", False):
+            tmp2 = self.__get_volume_label(file_path)
+            self.record_fx(f"[{file_path}] 的卷标是 [{tmp2}]")
+        else:
+            self.record_fx(f"检查卷标时出现错误 - {file_path} 文件不存在", tag=self.LOG_ERROR)
+            tmp2 = False
+        lab_blk = self.get_fromkey("label_blacklist", file_path)
+        if not lab_blk or tmp2 in lab_blk:
+            cur_exists_ch += 1 * 2 ** 6
+            cur_exists_list.update({"label_in_blacklist": True})
+        else:
+            cur_exists_ch += 0
+            cur_exists_list.update({"label_in_blacklist": False})
+        if tmp2 in self.get_fromkey("label_whitelist", file_path):
+            cur_exists_ch += 1 * 2 ** 7
+            cur_exists_list.update({"label_in_whitelist": True})
+        else:
+            cur_exists_ch += 0
+            cur_exists_list.update({"label_in_whitelist": False})
+
+        self.record_fx(f"{file_path} 编码的数字串 - {"%.16d" % int(dec_to_r_convert(cur_exists_ch, 2, ))}")
+        self.record_fx(f"{file_path} 字典 - {cur_exists_list}")
+        if return_type == "code":
+            return cur_exists_ch
+        else:
+            return cur_exists_list
+
 
 
 class SolidSync(BaseSynchronization):
-    pass
+    sync_type = "solid"
+
+    def run(self):
+        
 
 
 class CursorSync(BaseSynchronization):
-    pass
+    sync_type = "cursor"
 
 
 class ReplacementSync(BaseSynchronization):
-    pass
+    sync_type = "replacement"
 
 
 class DeviceSync(BaseSynchronization):
+    sync_type = "device"
+
+
+if __name__ == "__main__":
     pass
-
-
-if __name__ == '__main__':
-    g2 = logging.getLogger("abcc")
-    g2.setLevel(3)
-    print(g2.getEffectiveLevel())
-    g = BaseLogging("DFG")
-    g.setLevel(8)
-    g.debug("CNM")
-    g.error("CNNH")
-    g.notice("NOTICE")
